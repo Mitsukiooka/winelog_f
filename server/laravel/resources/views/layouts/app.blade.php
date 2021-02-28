dfa0c5e3261de71c61e8c2af3f753b84130d97e3
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2021-02-28 18:15:30 +0900

    design template

--- server/laravel/resources/views/layouts/app.blade.php
+++ server/laravel/resources/views/layouts/app.blade.php
@@ -1,23 +1,51 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
     <title>{{ config('app.name', 'Winelog') }}</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Favicons -->
+    <link href="{{ asset('img/favicon.png') }}" rel="icon">
+    <link href="{{ asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/icofont/icofont.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/animate.css/animate.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/owl.carousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/venobox/venobox.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/aos/aos.css') }}" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+
+
+    <!-- Template Main JS File -->
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor/jquery.easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('vendor/php-email-form/validate.js') }}"></script>
+    <script src="{{ asset('vendor/owl.carousel/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+    <script src="{{ asset('vendor/venobox/venobox.min.js') }}"></script>
+    <script src="{{ asset('vendor/aos/aos.js') }}"></script>
+
+    <!-- =======================================================
+    * Template Name: Restaurantly - v1.2.1
+    * Template URL: https://bootstrapmade.com/restaurantly-restaurant-template/
+    * Author: BootstrapMade.com
+    * License: https://bootstrapmade.com/license/
+    ======================================================== -->
 </head>
 <body>
     <div id="app">
@@ -84,3 +112,4 @@
     </div>
 </body>
 </html>
+
--- server/laravel/resources/views/wine/index.blade.php
+++ server/laravel/resources/views/wine/index.blade.php
@@ -65,6 +65,38 @@
                         {{ __('追加') }}
                     </button>
                 </div>
+                <section id="menu" class="menu section-bg">
+                    <div class="container" data-aos="fade-up">
+                        <div class="section-title">
+                        <h2>Wine Index</h2>
+                        <p>Check Your Wine Collection</p>
+                        </div>
+                        <div class="row" data-aos="fade-up" data-aos-delay="100">
+                        <div class="col-lg-12 d-flex justify-content-center">
+                            <ul id="menu-flters">
+                            <li data-filter="*" class="filter-active">All</li>
+                            <li data-filter=".filter-starters">Starters</li>
+                            <li data-filter=".filter-salads">Salads</li>
+                            <li data-filter=".filter-specialty">Specialty</li>
+                            </ul>
+                        </div>
+                        </div>
+                        @foreach ($wines as $wine)
+                            <div class="row menu-container" data-aos="fade-up" data-aos-delay="200">
+
+                                <div class="col-lg-6 menu-item filter-starters">
+                                    <img src="assets/img/menu/lobster-bisque.jpg" class="menu-img" alt="">
+                                    <div class="menu-content">
+                                    <a href="#">{{ $wine->name }}</a><span>{{ $wine->country }}</span>
+                                    </div>
+                                    <div class="menu-ingredients">
+                                    Lorem, deren, trataro, filede, nerada
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </section><!-- End Menu Section -->
             </div>
         </div>
     </div>
