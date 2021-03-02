47cabe78943e56c6958787b0d0bc4e6c6c646960
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2021-03-02 11:40:39 +0900

    update login page design

--- server/laravel/resources/views/auth/register.blade.php
+++ server/laravel/resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -73,5 +73,66 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
+
+<main id="main">
+    <section id="book-a-table" class="book-a-table">
+        <div class="container" data-aos="fade-up">
+            <div class="section-title">
+                <h2>Register</h2>
+            </div>
+
+            <form method="POST" action="{{ route('register') }}" role="form" class="php-email-form" data-aos="fade-up" data-aos-delay="100">
+            @csrf
+                <div class="form-row">
+                    <div class="col-lg-4 col-md-6 form-group">
+                        <input type="text" name="name" id="name" placeholder="Name" class="form-control @error('name') is-invalid @enderror" required autocomplete="name">
+                        <div class="validate"></div>
+                        @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="col-lg-4 col-md-6 form-group">
+                        <input type="text" name="email" id="email" placeholder="Email" class="form-control @error('email') is-invalid @enderror" required autocomplete="email">
+                        <div class="validate"></div>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="col-lg-4 col-md-6 form-group">
+                        <input type="password" name="password" id="password" placeholder="Password" class="form-control @error('password') is-invalid @enderror" required autocomplete="new-password">
+                        <div class="validate"></div>
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="col-lg-4 col-md-6 form-group">
+                        <input type="password" name="password_confirmation" id="password-confirm" placeholder="Password Confirm" class="form-control" required autocomplete="new-password">
+                        <div class="validate"></div>
+                    </div>
+                </div>
+                <div class="mb-3">
+                    <div class="loading">Loading</div>
+                    <div class="error-message"></div>
+                    <div class="sent-message">Your booking request was sent. We will call back or send an Email to confirm your reservation. Thank you!</div>
+                </div>
+                <div class="form-group row mb-0">
+                    <div class="col-md-6 offset-md-4">
+                        <button type="submit" class="btn btn-primary" name='action' value='add'>
+                            {{ __('登録') }}
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+</main>
 @endsection
