ff57e49a1b8d4b6314ced318c0d1532078024be2
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2021-03-03 17:33:24 +0900

    update favicon

--- server/laravel/resources/views/layouts/app.blade.php
+++ server/laravel/resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="{{ asset('img/favicon.png') }}" rel="icon">
+    <link href="{{ asset('img/icons8-wine-bottle-64.png') }}" rel="icon">
     <link href="{{ asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
     <!-- Google Fonts -->
