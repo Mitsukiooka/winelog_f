86c8e873f6c441a0ac070aaf43bc0b6e9cf86b34
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2021-03-05 21:37:01 +0900

    Update MakerController.php

--- server/laravel/app/Http/Controllers/MakerController.php
+++ server/laravel/app/Http/Controllers/MakerController.php
@@ -51,10 +51,11 @@ class MakerController extends Controller
             $maker = new \App\Maker;
             if ($request->hasFile('image_file')) {
                 $file = $request->file('image_file');
+                $name = $file->getClientOriginalName();
                 $image = Image::make($file)->resize(540, 400);
                 $path = Storage::disk('s3')->put('/', $image, 'public');
+                Storage::put(config('filesystems.s3.url').$name, (string) $image->encode());
                 $maker->image_file = Storage::disk('s3')->url($path);
-
 
             }
             $maker->name = $request->name;
