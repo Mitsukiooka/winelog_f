55e834f57acb6387af9a0b6b3af5e1e65a963df2
Mitsukiooka <66833185+Mitsukiooka@users.noreply.github.com>
2021-03-05 09:04:33 +0900

    Update web.php

--- server/laravel/routes/web.php
+++ server/laravel/routes/web.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::resource('wine', 'wineController');
-Route::resource('maker', 'makerController');
-Route::resource('profile', 'profileController', ['only' => ['show', 'create', 'store', 'edit', 'update', 'destroy']]);
+Route::resource('wine', 'WineController');
+Route::resource('maker', 'MakerController');
+Route::resource('profile', 'ProfileController', ['only' => ['show', 'create', 'store', 'edit', 'update', 'destroy']]);
 
 Route::get('/','HomeController@index');
 Route::get('home', 'HomeController@index');
